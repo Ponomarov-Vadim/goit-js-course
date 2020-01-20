9dc6d635092a52c7311ee28490a7f77cc3dce9b1
Vadim Ponomarov <remary300@gmail.com>
2020-01-20 21:32:06 +0200

    added changes to task 2

--- javascript/homework-01/src/task-2.js
+++ javascript/homework-01/src/task-2.js
@@ -2,12 +2,8 @@
 const total = 80;
 let ordered = 20;
 
-for (let index = 0; index < 3; index++) {
-  if (total >= ordered) {
-    console.log("Заказ оформлен, с вами свяжется менеджер");
-  } else {
-    console.log("На складе недостаточно твоаров!");
-  }
-
-  ordered += 40;
+if (total >= ordered) {
+  console.log("Заказ оформлен, с вами свяжется менеджер");
+} else {
+  console.log("На складе недостаточно твоаров!");
 }
