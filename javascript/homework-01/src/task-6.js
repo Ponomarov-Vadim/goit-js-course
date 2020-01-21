10b19208e1bdee7bb3bae1eb55c01c45a604ac95
Vadim Ponomarov <remary300@gmail.com>
2020-01-21 22:30:36 +0200

    finish task 6

--- javascript/homework-01/src/task-6.js
+++ javascript/homework-01/src/task-6.js
@@ -6,8 +6,8 @@ do {
   input = prompt("Enter the number");
 
   if (input !== null) {
-    if (!Number.isNaN(Number(input))) {
-      total += Number.parseInt(input);
+    if (!Number.isNaN(+input)) {
+      total += +input;
     } else {
       alert("Было введено не число, попробуйте еще раз");
     }
