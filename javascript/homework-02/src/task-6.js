815510a0a877c02c9ed5ab2f681db1282b1211a3
Vadim Ponomarov <remary300@gmail.com>
2020-01-21 22:34:03 +0200

    added changes task 6

--- javascript/homework-02/src/task-6.js
+++ javascript/homework-02/src/task-6.js
@@ -7,17 +7,17 @@ let total = 0;
 do {
   input = prompt("Enter the number");
   if (input !== null) {
-    if (isNaN(input)) {
+    if (Number.isNaN(+input)) {
       alert("Было введено не число, попробуйте еще раз");
     } else {
-      numbers.push(input);
+      numbers.push(+input);
     }
   }
 } while (input !== null);
 
 if (numbers.length !== 0) {
   for (const num of numbers) {
-    total += Number(num);
+    total += num;
   }
 } else {
   alert("Массив чисел пуст!");
