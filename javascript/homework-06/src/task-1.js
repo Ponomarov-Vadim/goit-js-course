49c6636d3ecf67c29f99f267ea9c2645dd3deaf1
Vadim Ponomarov <remary300@gmail.com>
2020-01-27 12:55:38 +0200

    added changes to index and task

--- javascript/homework-06/src/task-1.js
+++ javascript/homework-06/src/task-1.js
@@ -1,68 +1,78 @@
 "use strict";
+import users from "./users.js";
+
 // Задание 1
 // Получить массив имен всех пользователей (поле name).
-
 const getUserNames = users => {
-  // твой код
+  return users.map(user => user.name);
 };
 
 console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head',
+//'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 // Задание 2
 // Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 
 const getUsersWithEyeColor = (users, color) => {
-  // твой код
+  return users.filter(user => user.eyeColor === color);
 };
 
-console.log(getUsersWithEyeColor(users, "blue")); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.log(getUsersWithEyeColor(users, "blue"));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 // Задание 3
 // Получить массив имен пользователей по полу (поле gender).
 
 const getUsersWithGender = (users, gender) => {
-  // твой код
+  return users.filter(user => user.gender === gender).map(user => user.name);
 };
 
-console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersWithGender(users, "male"));
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 // Задание 4
 // Получить массив только неактивных пользователей (поле isActive).
 
 const getInactiveUsers = users => {
-  // твой код
+  return users.filter(user => !user.isActive);
 };
 
-console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+console.log(getInactiveUsers(users));
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 // Задание 5
 // Получить пользоваля (не массив) по email (поле email, он уникальный).
 
 const getUserWithEmail = (users, email) => {
-  // твой код
+  return users.find(user => user.email === email);
 };
 
-console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
-console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
+console.log(getUserWithEmail(users, "shereeanthony@kog.com"));
+// {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, "elmahead@omatom.com"));
+// {объект пользователя Elma Head}
 
 // Задание 6
-// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+// Получить массив пользователей попадающих в возрастную категорию
+//от min до max лет (поле age).
 
 const getUsersWithAge = (users, min, max) => {
-  // твой код
+  return users.filter(user => user.age >= min && user.age <= max);
 };
 
-console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30));
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
 console.log(getUsersWithAge(users, 30, 40));
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson,
+// объект Sheree Anthony]
 
 // Задание 7
 // Получить общую сумму баланса (поле balance) всех пользователей.
 
 const calculateTotalBalance = users => {
-  // твой код
+  return users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
 };
 
 console.log(calculateTotalBalance(users)); // 20916
@@ -71,30 +81,56 @@ console.log(calculateTotalBalance(users)); // 20916
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
 const getUsersWithFriend = (users, friendName) => {
-  // твой код
+  return users
+    .filter(user => user.friends.some(friend => friend === friendName))
+    .map(user => user.name);
 };
 
-console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Briana Decker"));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+// [ 'Elma Head', 'Sheree Anthony' ]
 
 // Задание 9
-// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+// Массив имен (поле name) людей, отсортированных в зависимости
+//от количества их друзей (поле friends)
 
 const getNamesSortedByFriendsCount = users => {
-  // твой код
+  return users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(user => user.name);
 };
 
 console.log(getNamesSortedByFriendsCount(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr',
+// 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 // Задание 10
 // Получить массив всех умений всех пользователей (поле skills),
-//при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+//при этом не должно быть повторяющихся умений и они должны быть
+// отсортированы в алфавитном порядке.
 
 const getSortedUniqueSkills = users => {
-  // твой код
+  const allSkills = (allSkills, user) => {
+    allSkills.push(...user.skills);
+    return allSkills;
+  };
+
+  const uniqueSkills = (uniqueSkills, skill) => {
+    if (uniqueSkills.indexOf(skill) === -1) {
+      uniqueSkills.push(skill);
+    }
+    return uniqueSkills;
+  };
+
+  return users
+    .reduce(allSkills, [])
+    .reduce(uniqueSkills, [])
+    .sort();
 };
 
 console.log(getSortedUniqueSkills(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure',
-// 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit',
+// 'ex', 'ipsum', 'irure',
+// 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla',
+//'proident', 'tempor', 'velit', 'veniam' ]
